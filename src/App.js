3364deba6357ccb256829c550f7e4d2d9f5959f0
Fluturecode <fluturecode@gmail.com>
2022-12-26 12:01:14 -0500

    updated the sendGif function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import { useEffect, useState } from 'react';
-import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
-import { Button } from 'react-bootstrap';
-import { render } from '@testing-library/react';
+import twitterLogo from './assets/twitter-logo.svg';
 
 const TWITTER_HANDLE = 'fluturecode';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const TEST_GIFS = [
-	'https://giphy.com/clips/justin-dancing-80s-mashup-XOmNbD0XLaAjCWZrDb',
-	'https://giphy.com/clips/episode-21-bet-soul-train-r9YHYHP6KoYZiRBQnu',
-	'https://giphy.com/gifs/whitneyhouston-0gm9UQR9tUDXxWAyUp',
-	'https://giphy.com/clips/animation-pink-design-JMDHyBwy1b57fz0Kub',
-  'https://giphy.com/gifs/80s-music-bangles-susanna-hoffs-v8xmVfox6XOq4'
-]
+  'https://giphy.com/clips/justin-dancing-80s-mashup-XOmNbD0XLaAjCWZrDb',
+  'https://giphy.com/clips/episode-21-bet-soul-train-r9YHYHP6KoYZiRBQnu',
+  'https://giphy.com/gifs/whitneyhouston-0gm9UQR9tUDXxWAyUp',
+  'https://giphy.com/clips/animation-pink-design-JMDHyBwy1b57fz0Kub',
+  'https://giphy.com/gifs/80s-music-bangles-susanna-hoffs-v8xmVfox6XOq4',
+];
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState();
@@ -24,20 +22,15 @@ const App = () => {
     if (window?.solana?.isPhantom) {
       console.log('Phantom wallet found!');
       const res = await window.solana.connect({ onlyIfTrusted: true });
-      console.log(
-        'Connected with Public Key:',
-        res.publicKey.toString()
-      );
+      console.log('Connected with Public Key:', res.publicKey.toString());
 
       setWalletAddress(res.publicKey.toString());
-
     } else {
       alert('Solana object not found! Get a Phantom Wallet 👻');
     }
-
   };
 
-  const connectWallet = async() => {
+  const connectWallet = async () => {
     const { solana } = window;
 
     if (solana) {
@@ -50,6 +43,8 @@ const App = () => {
   const sendGif = async () => {
     if (inputValue.length > 0) {
       console.log('Gif link:', inputValue);
+      setGifList([...gifList, inputValue]);
+      setInputValue('');
     } else {
       console.log('Empty input. Try again.');
     }
@@ -61,32 +56,32 @@ const App = () => {
   };
 
   const renderNotConnectedContainer = () => (
-    <div className="connected-container">
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        sendGif();
-      }}
-    >
-      <input
-        type="text"
-        placeholder="Enter gif link!"
-        value={inputValue}
-        onChange={onInputChange}
-      />
-      <button type="submit" className="cta-button submit-gif-button">
-        Submit
-      </button>
-    </form>
-    <div className="gif-grid">
-      {TEST_GIFS.map((gif) => (
-        <div className="gif-item" key={gif}>
-          <img src={gif} alt={gif} />
-        </div>
-      ))}
+    <div className='connected-container'>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          sendGif();
+        }}
+      >
+        <input
+          type='text'
+          placeholder='Enter gif link!'
+          value={inputValue}
+          onChange={onInputChange}
+        />
+        <button type='submit' className='cta-button submit-gif-button'>
+          Submit
+        </button>
+      </form>
+      <div className='gif-grid'>
+        {TEST_GIFS.map((gif) => (
+          <div className='gif-item' key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-  )
+  );
 
   useEffect(() => {
     const onLoad = async () => {
@@ -96,31 +91,28 @@ const App = () => {
     return () => window.removeEventListener('load', onLoad);
   }, []);
 
-
-useEffect(() => {
-  if (walletAddress) {
-    console.log('Fetching GIF list...');
-    setGifList(TEST_GIFS);
-  }
-}, [walletAddress]);
+  useEffect(() => {
+    if (walletAddress) {
+      console.log('Fetching GIF list...');
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
 
   return (
-    <div className="App">
+    <div className='App'>
       <div className={walletAddress ? 'authed-container' : 'container'}>
-        <div className="header-container">
-          <p className="header">🖼 80s Music GIFs</p>
-          <p className="sub-text">
-            Find your favorite 80's music video GIF ✨
-          </p>
+        <div className='header-container'>
+          <p className='header'>🖼 80s Music GIFs</p>
+          <p className='sub-text'>Find your favorite 80's music video GIF ✨</p>
           {!walletAddress && renderNotConnectedContainer()}
         </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+        <div className='footer-container'>
+          <img alt='Twitter Logo' className='twitter-logo' src={twitterLogo} />
           <a
-            className="footer-text"
+            className='footer-text'
             href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
+            target='_blank'
+            rel='noreferrer'
           >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
